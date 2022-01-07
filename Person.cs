342125155994e47c1a8722f796fe5e569c60350c
anagparedes <anagparedesg@gmail.com>
2022-01-07 19:29:13 -0400

    [ADD] test methods

--- Person.cs
+++ Person.cs
@@ -23,6 +23,27 @@ namespace RandomTest
             Person result = Ana;
             Assert.AreEqual(Ana, result);
         }
+        [TestMethod]
+        public void TestBoundaryName()
+        {
+            Person Ana = new Person("Nathalie", "Lopez", 21);
+            Person result = Ana;
+            Assert.AreEqual(Ana, result);
+        }
+        [TestMethod]
+        public void TestBoundaryLastName()
+        {
+            Person Ana = new Person("Pablo", "Martinez", 21);
+            Person result = Ana;
+            Assert.AreEqual(Ana, result);
+        }
+        [TestMethod]
+        public void TestBoundaryAge()
+        {
+            Person Ana = new Person("Monica", "Ricart", 21);
+            Person result = Ana;
+            Assert.AreEqual(Ana, result);
+        }
 
     }
 
