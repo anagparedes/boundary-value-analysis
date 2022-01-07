cc9db5c09bfc952511ae9c52f7419f95bcc7fac5
anagparedes <anagparedesg@gmail.com>
2022-01-07 18:55:17 -0400

    [ADD] first commit

---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RandomTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
